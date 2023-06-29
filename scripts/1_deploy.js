d1bd5dbdb3156237e88638099a57a098695dd3db
LasseJ <lasse.juusela@gmail.com>
2023-06-29 16:51:04 -0300

    2. Smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,14 @@
+async function main() {
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  // Deploy contract
+  const token = await Token.deploy()
+  await token.deployed()
+  console.log(`Token Deployed to: ${token.address}`)
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
