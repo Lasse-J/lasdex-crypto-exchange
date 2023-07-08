0565d5d70a6ce422419785fd75f2a71fdd844cb3
LasseJ <lasse.juusela@gmail.com>
2023-07-08 11:20:22 -0300

    20. Deposits pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets'
+import Balance from './Balance'
 
 function App() {
   const dispatch = useDispatch()
@@ -58,8 +60,10 @@ function App() {
 
     // Load Exchange Smart Contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
 
+    // Listen to events
+    subscribeToEvents(exchange, dispatch)
   }
 
   useEffect(() => {
@@ -76,7 +80,7 @@ function App() {
 
           <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
@@ -101,3 +105,4 @@ function App() {
 }
 
 export default App;
+ 
